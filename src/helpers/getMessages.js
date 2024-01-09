b0ed23c6517e691b55cf3735872cce538924bdf6
Alex <alex@alex-dev.me>
2024-01-09 02:18:22 -0600

    Add calendarLocalizer and getMessages helpers

---
+++ src/helpers/getMessages.js
@@ -0,0 +1,19 @@
+
+export const getMessagesES = () => {
+    return {
+        allDay: 'All Day',
+        previous: '<',
+        next: '>',
+        today: 'Today',
+        month: 'Month',
+        week: 'Week',
+        day: 'Day',
+        agenda: 'Schedule',
+        date: 'Date',
+        time: 'Hour',
+        event: 'Event',
+        noEventsInRange: 'There are no events in this range.',
+        showMore: total => `+ See more (${total})`
+    };
+}
+
