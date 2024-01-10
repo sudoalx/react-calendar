8d3b80229f27d36e73f1337cd4b2eef66602483b
Alex <alex@alex-dev.me>
2024-01-09 19:54:49 -0600

    Update Navbar.jsx to display user's name

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
   return (
     <div className="navbar navbar-dark bg-dark mb-4 px-4">
       <span className="navbar-brand">
-        <i className="fas fa-calendar-alt" /> Alex
+        <i className="fas fa-calendar-alt" /> {user.name}
       </span>
 
       <button className="btn btn-outline-danger" onClick={startLogout}>
